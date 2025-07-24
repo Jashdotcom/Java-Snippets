b0eea5764716f5fd5a81b3a3d84357f1be63eef3
Jashdotcom <jashmistry23@gmail.com>
2025-07-24 16:30:42 +0530

    Add files via upload

---
+++ SquareMethod.java
@@ -0,0 +1,14 @@
+public class SquareMethod {
+
+    //Creating a method to square a number
+
+    public static void main(String[] args){
+
+        System.out.println(square(4));
+
+    }static double square(double number){
+
+            return Math.pow(number,2);
+        }
+
+    }
